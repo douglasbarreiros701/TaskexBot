866c58896fe9709a5d777f5e24dff1638a48bbfd
Douglas Barreiros <Douglasb701@gmail.com>
2023-01-09 21:01:55 -0300

    primeiro commit

---
+++ commands/ping.js
@@ -0,0 +1,14 @@
+const execute = (bot, msg, args)=>{
+    return msg.reply("Pong")
+}
+
+module.exports = {
+    name: "ping",
+    ping: "ping",
+    execute,
+
+}
+
+
+
+    
---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js")
+const config = require("./config.json");
+const fs = require("fs")
+const path = require("path")
+
+const bot = new Discord.Client({
+    intents: [8]
+})
+
+bot.login(config.BOT_TOKEN)
+bot.commands = new Discord.Collection()
+
+const prefix = "!"
+
+// pasta commands
+const commandFiles = fs
+    .readdirSync(path.join(__dirname, "./commands/"))
+    .filter((filename)=> filename.endsWith(".js"))
+
+    console.log(commandFiles)
+
+
+for (var filename of commandFiles){
+    const command = require(`./commands/${filename}`)
+    bot.commands.set(command.name, command)
+}
+
+/******************************************************************* */
+
+// config do bot
+bot.on("message", function(msg){
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return
+
+    const args = msg.content.slice(prefix.length).split(); 
+    console.log(args)
+    const command = args.shift()
+
+    try{
+        bot.commands.get(command).execute(bot, msg, args)
+        
+    }catch (err){
+        console.error(err)
+        return msg.reply("Ops! Ainda não conheço esse comando")
+    }
+   
+})
+
+/******************************************************************* */
+    
+bot.on("ready", ()=>{
+    console.log(`Estou conectado como ${bot.user.username}`)
+})
