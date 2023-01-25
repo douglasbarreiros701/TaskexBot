edb7784287f43906ebe3b5705e8b63e5df64933a
Izaias Henrique <izaias.rique@gmail.com>
2023-01-25 11:25:48 -0300

    feat: configure dotenv and change config.json to .env file

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
-const fs = require("fs");
-const path = require("path");
 const mongoose = require("mongoose")
-const db = require("./src/config/database")
-
-
-
 mongoose.set("strictQuery", true);
+
+require('dotenv').config()
+
+//Environment variables verification
+if(!process.env.DATABASE_URL || !process.env.DISCORD_BOT_TOKEN) 
+  throw new Error('A variável de ambiente DATABASE_URL ou DISCORD_BOT_TOKEN é indefinida.')
+
+
+const db = require("./src/config/database")
 db.connect();
 
-const bot = new Discord.Client({
+const discord = require("discord.js");
+const bot = new discord.Client({
   intents: [8],
 });
 
 
-bot.login(config.BOT_TOKEN);
-
+bot.login(process.env.DISCORD_BOT_TOKEN);
 
 const prefix = "!";
-
 
 bot.on("message", function (msg) {
   if (!msg.content.startsWith(prefix)) return;
